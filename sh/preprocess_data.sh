69b6b9ba3a464f35d48251e5abddd76f546f22cf
kosuke futamata <f-e@toki.waseda.jp>
2018-08-04 08:34:59 +0900

    fix it

--- sh/preprocess_data.sh
+++ sh/preprocess_data.sh
@@ -1,2 +1,2 @@
-python preprocess_tokens.py small_parallel_enja/train.ja data/ja_dataset.pickle --lang jp --tokenize --val_in_path small_parallel_enja/dev.ja --val_out_path data/ja_dataset_dev.pickle
-python preprocess_tokens.py small_parallel_enja/train.en data/en_dataset.pickle --lang en --tokenize --val_in_path small_parallel_enja/dev.en --val_out_path data/en_dataset_dev.pickle
+python src/data_preparation/preprocess_tokens.py small_parallel_enja/train.ja data/ja_dataset.pickle --lang jp --tokenize --val_in_path small_parallel_enja/dev.ja --val_out_path data/ja_dataset_dev.pickle
+python src/data_preparation/preprocess_tokens.py small_parallel_enja/train.en data/en_dataset.pickle --lang en --tokenize --val_in_path small_parallel_enja/dev.en --val_out_path data/en_dataset_dev.pickle
